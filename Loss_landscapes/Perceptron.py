6ffb72816edc077e26b1b39d7055ca076671cf5a
orodrigoaraizabravo <71989729+orodrigoaraizabravo@users.noreply.github.com>
2023-03-22 17:25:01 -0400

    Update Perceptron.py

--- Loss_landscapes/Perceptron.py
+++ Loss_landscapes/Perceptron.py
@@ -11,12 +11,13 @@ from CNODES import EDNN
 def Perceptron_ansatz(N, h=0.5): 
     Gs = [['z', 'z', i,N-1] for i in range(N-2)]
     Fs = [['x', i] for i in range(N)]
+    Fs += [['z', i] for i in range(N)]
     Hout = [[1.0, 'z', 'z', i,i+1] for i in range(N-1)]
     Hout+= [[h, 'x', i] for i in range(N)]
     return Gs, Fs, Hout
 
 N=5
-times=torch.linspace(0,1,50)
+times=torch.linspace(0,1,20)
 
 Gs, Fs, Hout= Perceptron_ansatz(N)
 perceptron=EDNN(N, Gs, Fs, Hout)
