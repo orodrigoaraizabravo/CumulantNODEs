0132d7136cc8eac0e89c6d8126002186dff45a36
orodrigoaraizabravo <71989729+orodrigoaraizabravo@users.noreply.github.com>
2023-03-24 12:15:45 -0400

    Update CNODEs.py

--- CNODEs.py
+++ CNODEs.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Feb 27 14:41:45 2023
 
-@author: Rodrigo Araiza Bravo
+@author: HP
 """
 
 import torch 
@@ -10,6 +10,7 @@ from torch import nn, pi, optim
 from torchdiffeq import odeint, odeint_adjoint
 from torch.special import legendre_polynomial_p as lp
 import numpy as np
+import pickle 
 
 def call_operators(N, Graph, Fields, Main): 
     Main.eval("""σ(i, axis) = Sigma(h,Symbol(:σ_,i), axis, i)""")
@@ -47,6 +48,33 @@ def call_Hout(Hout):
         ops.append(''.join(["σ"+cop[i]+"("+str(cop[oplen+i])+")*" for i in range(oplen)])[:-1])
     return ops
 
+def output_as_string(N,order,Hout,varmap_out):
+    from julia import Main
+    Main.using("QuantumCumulants")
+    Main.using("ModelingToolkit")
+    Main.using("Symbolics")
+    Main.eval("""σ(i, axis) = Sigma(h,Symbol(:σ_,i), axis, i)""")
+    Main.eval("σx(i) = σ(i, 1)")
+    Main.eval("σy(i) = σ(i, 2)")
+    Main.eval("σz(i) = σ(i, 3)")
+    Main.eval("N="+str(N))
+    Main.eval("order="+str(order))
+    Main.eval("h = ⊗([SpinSpace(Symbol(:Spin,i)) for i=1:N]...)")
+    Outops = call_Hout(Hout)
+    
+    sHout = 'Hout='
+    for i in range(len(Hout)): sHout+=str(Hout[i][0])+'*'+Outops[i]+'+'
+        
+    Main.eval(sHout[:-1])
+    Main.eval("""avgout = cumulant_expansion(average(Hout),order)""")
+    
+    s=str(Main.avgout)[15:-1]
+    for x in [str(integer) for integer in range(1,10)]: s=s.replace(x+'var', x+'*var').replace(x+'⟨', x+'*⟨')
+    for key in varmap_out: 
+            s=s.replace(key, varmap_out[key])
+    for x in [str(integer) for integer in range(0,10)]: s=s.replace(x+'x', x+'*x')
+    return s.replace("⟨","").replace("⟩","").replace("^","**").replace("*)",")").replace(')self',')*self')
+
 def call_Main(N, Graph, Fields, Hout, order,Main):
     Main.using("QuantumCumulants")
     Main.using("ModelingToolkit")
@@ -55,62 +83,115 @@ def call_Main(N, Graph, Fields, Hout, order,Main):
     Main.eval("order="+str(order))
     Main.eval("h = ⊗([SpinSpace(Symbol(:Spin,i)) for i=1:N]...)")
     Jops, hops = call_operators(N,Graph, Fields,Main)
-    Outops = call_Hout(Hout)
 
     
     Js, hs = call_constants(N, Graph, Fields,Main)
+    
+    Outops = call_Hout(Hout)
+    sops = 'ops=['
+    for i in range(len(Hout)): sops +=Outops[i]+','
+        
+    sops=sops[:-1]+']'
+    Main.eval(sops)
     
     sH='H='
     for i in range(len(Graph)): sH+=Js[i]+'*'+Jops[i]+'+'
     for i in range(len(Fields)): sH+=hs[i]+'*'+hops[i]+'+'
     
-    sHout = 'Hout='
-    sops = 'ops=['
-    for i in range(len(Hout)): 
-        sHout+=str(Hout[i][0])+'*'+Outops[i]+'+'
-        sops +=Outops[i]+','
-    sops=sops[:-1]+']'
     Main.eval(sH[:-1])
-    Main.eval(sHout[:-1])
-    Main.eval(sops)
     Main.eval("""eqs = meanfield(ops, H; order=order)""")
     Main.eval("""eqs = complete(eqs, order=order)""")
     Main.eval("""sys=ODESystem(eqs; name=:cumeqs)""")
-    Main.eval("""avgout = cumulant_expansion(average(Hout),order)""")
-    return Jops, hops
+    return
 
+def Make_Ansatz(N, Gs, Fs, Ops, order, name):
+    """This is a very important function which takes in the graph and fields of a 
+    Hamiltonian ansatz (Gs,Fs) of a certain size (N) and creates the cumulant 
+    expansion of a certain order (order). Ops contains information about the kind of 
+    expectation values one wishes to be expressed in the ODEs. 
+    
+    We advise that the Ansatz of a certain geometry and order is only made once 
+    since this procedure has a runtime of O(N!). Thereafter, the ansatz is stored in a 
+    file called name.pt. 
+    The Ansatz is then loaded into the Cumulant NN. In this sense, the loading should only take 
+    a runtime of O(N^k)."""
+    from julia import Main
+    #The following line builds the ansatz in Julia language
+    call_Main(N, Gs, Fs, Ops, order, Main)
+    
+    #Now, we need to parse it and produce a dictionary 
+    
+    #Get variable and parameter maps
+    varmap = {str(Main.sys.states[i])[15:-1]:"x"+"["+str(i)+"]"\
+                   for i in range(len(Main.sys.states))}
+    parmap = {str(Main.sys.ps[i])[15:-1]:'' for i in range(len(Main.sys.ps))}
+    
+    cntJ=0
+    cnth=0
+    for key in parmap.keys(): 
+        if 'J' in key: 
+            parmap[key]='self.J('+str(cntJ)+')'
+            cntJ+=1
+        else: 
+            parmap[key]='self.p('+str(cnth)+',t)'
+            cnth+=1
+            
+    #Getting the variable map for the output
+    varmap_out = {}
+    for key in varmap:
+            newkey = key.replace("var","").replace("(t)","")[1:-1]
+            varmap_out[newkey] = varmap[key]
+    
+    #Parsing the equations to make them into python-compatible strings
+    def parse_string(s):
+        s=s.replace("im","0").replace(" ","").replace(")J",")*J")
+        s=s.replace(')var',')*var')
+        for key in varmap.keys(): 
+            s=s.replace(key, varmap[key])
+        for key in parmap.keys():
+            s=s.replace(key, parmap[key])
+            
+        for x in [str(integer) for integer in range(1,10)]: 
+            s=s.replace(x+'self',x+'*self').replace(x+'x', x+'*x')
+        return s.replace("⟨","").replace("⟩","").replace("^","**").replace("*)",")").replace(')self',')*self')
+    
+    equations_as_strings = [parse_string(str(Main.sys.eqs[i].rhs)[15:-1]) for i in range(len(Main.sys.eqs))]
+    
+    dic = {'Variable map': varmap, 
+           'Parameter map': parmap, 
+           'Variable map for output': varmap_out,
+           'Equations': equations_as_strings, 
+           'Operator base': Ops,
+           'Graph': Gs,
+           'Fields': Fs,
+           'N': N, 
+           'order': order}
+    
+    with open(name, 'wb') as outfile:
+        pickle.dump(dic, outfile)
+    print('Ansatz saved!')
+    
+    return 
+    
 class CumulantNN(nn.Module):
-    def __init__(self, N, Graph, Fields, order, Hout, Main, recall=False, T=1.0, n_basis = 5, basis='Fourier', x0=None, adjoint=False, Js=None, vs=None,method='euler'):
+    def __init__(self, N, file, Hout, T=1.0, n_basis = 5, basis='Fourier', x0=None, adjoint=False, Js=None, vs=None,method='euler'):
         super(CumulantNN, self).__init__()
-        self.Main=Main
-        if recall:
-            _=call_Main(N, Graph, Fields,Hout, order, Main)
-        self.order=order
-        self.n_qubits=N
+        with open(file, 'br') as infile:
+            self.dic = pickle.load(infile)
+    
+        self.n_qubits=self.dic['N']
+        self.order=self.dic['order']
+        self.varmap = self.dic['Variable map']
+        self.varmap_out= self.dic['Variable map for output']
+        self.parmap = self.dic['Parameter map']
+        self.Graph   = self.dic['Graph']
+        self.Fields  = self.dic['Fields']
+        self.equations_as_strings = self.dic['Equations']
+        
         self.n_basis= n_basis
         self.basis = basis
         self.method=method
         self.T=T
-        self.varmap = {str(self.Main.sys.states[i])[15:-1]:"x"+"["+str(i)+"]"\
-                       for i in range(len(self.Main.sys.states))}
-        self.get_varmap_out()
-        #self.complete_varmap_out()
-        #self.set_output()
-        
-        self.parmap = {str(self.Main.sys.ps[i])[15:-1]:'' for i in range(len(self.Main.sys.ps))}
-        
-        cntJ=0
-        cnth=0
-        for key in self.parmap.keys(): 
-            if 'J' in key: 
-                self.parmap[key]='self.J('+str(cntJ)+')'
-                cntJ+=1
-            else: 
-                self.parmap[key]='self.p('+str(cnth)+',t)'
-                cnth+=1
-        
-        self.Graph   = Graph
-        self.Fields  = Fields
         
         if Js is None: self.Js  = nn.Parameter(torch.rand(len(self.Graph)))
         else: self.Js = nn.Parameter(Js)
@@ -118,23 +199,16 @@ class CumulantNN(nn.Module):
         if vs is None: self.vs =  nn.Parameter(torch.rand([len(self.Fields), n_basis]))
         else: self.vs = nn.Parameter(vs)
         
-        if x0 is None: self.x0 = torch.zeros(len(self.Main.sys.states))
+        if x0 is None: self.exps_to_vect([[0.0,0.0,1.0] for i in range(self.n_qubits)])
         else: self.x0=x0
+        
         self.odeint = odeint_adjoint if adjoint else odeint
         
-        if basis not in ['Legendre', 'Fourier', 'poly']:
-            raise ValueError('Basis ' + basis + ' not supported')
+        if basis not in ['Legendre', 'Fourier', 'poly']: raise ValueError('Basis ' + basis + ' not supported')
         
         self.get_equations()
-        self.get_string_out()
+        self.s = eval('lambda x:' + output_as_string(self.n_qubits,self.order,Hout,self.varmap_out))
         
-    def get_varmap_out(self):
-        self.varmap_out = {}
-        for key in self.varmap:
-            newkey = key.replace("var","").replace("(t)","")[1:-1]
-            self.varmap_out[newkey] = self.varmap[key]
-        return 
-    
     def J(self,i):
         return (2*torch.sigmoid(self.Js[i])-1)
     
@@ -150,40 +224,14 @@ class CumulantNN(nn.Module):
             
         return (2*torch.sigmoid(u)-1)
     
-    
-    def parse_string(self,s):
-        for x in [" "+str(integer) for integer in range(1,10)]: s=s.replace(x,x+'*').replace(x[1:]+'var', x[1:]+'*var')
-        s=s.replace("im","").replace(" ","").replace(")J",")*J")
-        for key in self.varmap: 
-            s=s.replace(key, self.varmap[key])
-        for key in self.parmap:
-            s=s.replace(key, self.parmap[key])
-        return s.replace("⟨","").replace("⟩","").replace("^","**").replace("*)",")").replace(')self',')*self')
-    
-    def parse_string_out(self):
-        s=str(self.Main.avgout)[15:-1]
-        for x in [" "+str(integer) for integer in range(1,10)]: s=s.replace(x,x+'.*').replace(x[1:]+'var', x[1:]+'*var').replace(x[1:]+'⟨', x[1:]+'*⟨')
-        for key in self.varmap_out: 
-            s=s.replace(key, self.varmap_out[key])
-        return s.replace("⟨","").replace("⟩","").replace("^","**").replace("*)",")").replace(')self',')*self')
-    
-    def _equation(self, i):
-        s=str(self.Main.sys.eqs[i].rhs)[15:-1]
-        return self.parse_string(s)
-    
     def get_equations(self):
         self.equations= [] 
         for i in range(len(self.varmap)):
-            self.equations.append(eval('lambda self, t, x: '+self._equation(i)))
-        return 
-    
-    def get_string_out(self):
-        self.s = eval('lambda x: '+self.parse_string_out())
+            self.equations.append(eval('lambda self, t, x: '+self.equations_as_strings[i].replace('j','')))
         return 
     
     def func(self,t,x):
         dx_dt = torch.zeros(x.size()[0])
-        #ps = [self.p(i,t) for i in range(len(self.Fields))]
         for i in range(x.size()[0]): dx_dt[i] = self.equations[i](self,t,x) #function of time
         return dx_dt
     
@@ -235,8 +283,8 @@ class EDNN(nn.Module):
     def __init__(self, N, Graph, Fields, Hout, T=1.0, n_basis = 5, basis='Fourier', x0=None, Js=None, vs=None, method='euler'):
         super(EDNN,self).__init__()
         self.N=N
-        self.Gs=Graph
-        self.Fs=Fields
+        self.Graph=Graph
+        self.Fields=Fields
         self.Hout=Hout
         self.T=T
         self.n_basis=n_basis
@@ -244,10 +292,10 @@ class EDNN(nn.Module):
         self.method=method
         
         if Js is None: self.Js  = nn.Parameter(torch.rand(len(self.Graph)))
-        else: self.Js = nn.Parameter(Js)
+        else: self.Js = nn.Parameter(Js,requires_grad=True)
         
         if vs is None: self.vs =  nn.Parameter(torch.rand([len(self.Fields), n_basis]))
-        else: self.vs = nn.Parameter(vs)
+        else: self.vs = nn.Parameter(vs,requires_grad=True)
         
         if x0 is None: 
             self.x0 = torch.zeros(2**self.N, dtype=torch.cfloat)
@@ -287,12 +335,12 @@ class EDNN(nn.Module):
         
     def _evolving_H(self): 
         self.Gops=[]
-        for term in self.Gs: 
+        for term in self.Graph: 
             ops = [self.get_op(term[i]) for i in range(len(term)//2)]
             sites = term[len(term)//2:]
             self.Gops.append(self.add_op(ops, sites))
         self.Fops=[]
-        for term in self.Fs: 
+        for term in self.Fields: 
             op, site = [self.get_op(term[0])], term[1:]
             self.Fops.append(self.add_op(op, site))
         return
